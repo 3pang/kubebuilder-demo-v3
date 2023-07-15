4820d01ace7b4cba0a0fb7997aed8f07352df8a8
3pang <3032139@qq.com>
2023-07-15 10:33:49 +0800

    测试验证2

--- cmd/main.go
+++ cmd/main.go
@@ -30,9 +30,11 @@ import (
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/healthz"
 	"sigs.k8s.io/controller-runtime/pkg/log/zap"
+	"sigs.k8s.io/controller-runtime/pkg/webhook"
 
 	ingressv1beta1 "github.com/kubebuilder-demo/api/v1beta1"
 	"github.com/kubebuilder-demo/internal/controller"
+	"github.com/kubebuilder-demo/workload"
 	//+kubebuilder:scaffold:imports
 )
 
@@ -98,7 +100,7 @@ func main() {
 		os.Exit(1)
 	}
 
-	// mgr.GetWebhookServer().Register("/validate-apps-v1-deployment", &webhook.Admission{Handler: &workload.DeployWrapper{Client: mgr.GetClient()}})
+	mgr.GetWebhookServer().Register("/validate-apps-v1-deployment", &webhook.Admission{Handler: &workload.DeployWrapper{Client: mgr.GetClient()}})
 	if err = (&ingressv1beta1.App{}).SetupWebhookWithManager(mgr); err != nil {
 		setupLog.Error(err, "unable to create webhook", "webhook", "App")
 		os.Exit(1)
